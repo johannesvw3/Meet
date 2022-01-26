f7546689df03d2449c885163d6fe1e61080d8a5f
Johannes van Woerkom <johannesvw3@gmail.com>
2022-01-26 01:54:02 -0500

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -1,11 +1,20 @@
 import React, {Component } from 'react';
 import Event from './Event';
+import { Row, Container } from 'react-bootstrap';
+import { WarningAlert } from './Alert';
 
 class EventList extends Component {
   render() {
     const { events } = this.props;
 
     return (
+      <Container className="eventlist-container">
+				<Row className="d-flex justify-content-center event-list">
+					{!navigator.onLine ? (
+						<WarningAlert text="You are offline, the events list has been loaded from the Cache!" />
+					) : (
+						''
+					)}
       <ul className="EventList">
         {events.map(event => 
           <li key={event.id}>
@@ -13,6 +22,8 @@ class EventList extends Component {
           </li>
         )}
       </ul>
+      </Row>
+      </Container>
     );
   };
 }
