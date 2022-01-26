7ea3d11f3851c8f4d85672366d21a204f64869ec
Johannes van Woerkom <johannesvw3@gmail.com>
2022-01-26 01:33:48 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -51,6 +51,13 @@ export const getEvents = async () => {
     NProgress.done();
     return mockData;
   }
+
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    // console.log(events);
+    return data ? JSON.parse(data).events:[];;
+  }
   const token = await getAccessToken();
 
   if (token) {
